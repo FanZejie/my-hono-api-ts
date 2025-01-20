ae1d109d8c71b89b48ef773642d982c1e8c69329
Fanzejie <fanzejiea@gmail.com>
2025-01-20 09:22:35 +0800

    add swagger ui

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
-import { Hono } from 'hono'
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
 import menu from './routes/menu'
 import analysis from './routes/analysis'
 
-const app = new Hono()
-app.get('/', (c) => c.text('Please test me!'))
+
+const app = new OpenAPIHono();
+app.doc("/doc", {
+    openapi: "3.0.0",
+    info: {
+        version: "1.0.0",
+        title: "My API",
+    },
+});
+app.get("/ui", swaggerUI({ url: "/doc" }));
 app.route('/api/menu', menu)
 app.route('/api/analysis', analysis)
 
--- src/routes/analysis/index.ts
+++ src/routes/analysis/index.ts
@@ -1,8 +1,29 @@
-import { Hono } from "hono";
+import { z } from "zod";
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 import fakeAlarmData from "../../fakeData/causeAnalysis";
 
-const analysis = new Hono()
+const analysis = new OpenAPIHono()
 
-analysis.get('/', (c) => c.json(fakeAlarmData))
+const basicRoute = createRoute({
+    method: "get",
+    path: "/",
+    responses: {
+        200: {
+            content: {
+                "application/json": {
+                    schema: z.object({
+                        alertTotal: z.number(),
+                        alertSix: z.number(),
+                        alertNoHandle: z.number(),
+                        alertHanding: z.number(),
+                    }),
+                },
+            },
+            description: "say hello",
+        },
+    },
+});
+
+analysis.openapi(basicRoute,(c) => c.json(fakeAlarmData))
 
 export default analysis
